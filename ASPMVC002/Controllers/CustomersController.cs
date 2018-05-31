ad7c48fe32489c4d85ac5547d4b5447bef77a6a2
EdenVerdugo <eden.verdugo@gmail.com>
2018-05-31 00:11:13 -0600

    Primer commit

---
+++ ASPMVC002/Controllers/CustomersController.cs
@@ -0,0 +1,44 @@
+﻿using ASPMVC002.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace ASPMVC002.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int Id)
+        {
+            var customer = _context.Customers.SingleOrDefault(x => x.Id == Id);
+
+            if (customer == null)
+                return HttpNotFound();        
+
+            return View(customer);
+        }
+        
+    }
+}
